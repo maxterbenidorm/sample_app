ba148f70a13e6f633961c1add640be357c1c1de0
David Antolin Gonzalez <maxterbenidorm@gmail.com>
2010-11-22 21:18:19 +0100

    Finalizado el layout y las rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   get 'pages/home'
   get 'pages/contact'
   get 'pages/about'
   get 'pages/help'
   
+  # Named routes
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
+  match '/signup', :to => 'users#new'
   
   root :to => 'pages#home'
 end
--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -5,7 +5,7 @@ describe "LayoutLinks" do
     get '/'
     response.should have_selector("title", :content => "Home")
   end
-  
+
   it "should have a Contact page at '/contact'" do
     get '/contact'
     response.should have_selector("title", :content => "Contact")
@@ -19,5 +19,10 @@ describe "LayoutLinks" do
   it "should have a Help page at '/help'" do
     get '/help'
     response.should have_selector("title", :content => "Help")
+  end
+  
+  it "should have a Sign up page at '/signup'" do
+    get '/signup'
+    response.should have_selector("title", :content => "Sign up")
   end
 end
