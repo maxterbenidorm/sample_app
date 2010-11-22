8c995c6864d2fbe63396e114903dc1ad0dae7f68
David Antolin Gonzalez <maxterbenidorm@gmail.com>
2010-11-22 11:47:04 +0100

    Añadido helper title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  # Retorna un título para la página HTML
+  def title
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if @title.nil? then
+      base_title
+    else
+      base_title + " | " + @title
+    end
+  end
+  
 end
